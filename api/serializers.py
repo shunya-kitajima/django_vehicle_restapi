3e64e3bdbdbbf496a9935fc295d5320c8d1be5f6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-30 15:11:50 +0900

    create UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from models import Segment, Brand, Vehicle
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True, "required": True, "min_length": 5}}
+
