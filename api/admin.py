a422a1edcae28c86b79a6e2ef9decf463630b8d2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-30 13:39:33 +0900

    import models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Segment, Brand, Vehicle
 
-# Register your models here.
+
+admin.site.register(Segment)
+admin.site.register(Brand)
+admin.site.register(Vehicle)
+
