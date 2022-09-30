37038c42db3e66bb285a640d60ddf389aeeb0719
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-30 15:50:06 +0900

    create ViewSets of User

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.response import Response
+from .serializers import UserSerializer, SegmentSerializer, BrandSerializer, VehicleSerializer
+from .models import Segment, Brand, Vehicle
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ProfileUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        response = {"message": "PUT method is not allowed"}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {"message: PATCH method is not allowed"}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
