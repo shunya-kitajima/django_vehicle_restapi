eb1483d0987df72ed782eebab9f55d843b5ce237
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-30 15:15:15 +0900

    override create user method

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True, "required": True, "min_length": 5}}
 
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
+
+
