942ccf2d3605f18f176c43dc082b6fcf73993e16
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-30 13:27:27 +0900

    create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Segment(models.Model):
+    segment_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.segment_name
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brand_name
+
+
