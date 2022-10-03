a6c02c9db371f14daae1018a8121396e4b9c6152
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-03 11:21:06 +0900

    create test of get userprofile

--- api/test_1_user.py
+++ api/test_1_user.py
@@ -3,11 +3,23 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework import status
 
-CREATE_USER_URL = "/api/create/"
 PROFILE_URL = "/api/profile/"
+CREATE_USER_URL = "/api/create/"
 TOKEN_URL = "/api/auth/"
 
 
 class AuthorizedUserApiTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(username="dummy", password="dummy_pw")
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_1_1_should_get_user_profile(self):
+        res = self.client.get(PROFILE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            "id": self.user.id,
+            "username": self.user.username,
+        })
+
