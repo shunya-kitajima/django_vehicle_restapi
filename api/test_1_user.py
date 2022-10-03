5435d34c9e4ec910335749d8133cf461c3d06e45
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-03 11:29:53 +0900

    modify line

--- api/test_1_user.py
+++ api/test_1_user.py
@@ -16,7 +16,6 @@ class AuthorizedUserApiTests(TestCase):
 
     def test_1_1_should_get_user_profile(self):
         res = self.client.get(PROFILE_URL)
-
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             "id": self.user.id,
@@ -29,7 +28,6 @@ class AuthorizedUserApiTests(TestCase):
             "password": "dummy_pw_",
         }
         res = self.client.put(PROFILE_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_1_3_should_not_allowed_by_PATCH(self):
@@ -38,5 +36,4 @@ class AuthorizedUserApiTests(TestCase):
             "password": "dummy_pw_",
         }
         res = self.client.patch(PROFILE_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
