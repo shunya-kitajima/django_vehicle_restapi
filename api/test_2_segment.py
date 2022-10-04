931930ea8b289afe7de1f2306fa424de4f54d43c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 11:10:41 +0900

    create test_2_1_should_get_all_segments

--- api/test_2_segment.py
+++ api/test_2_segment.py
@@ -16,3 +16,20 @@ def create_segment(segment_name):
 def detail_url(segment_id):
     return reverse("api:segment-detail", args=[segment_id])
 
+
+class AuthorizedSegmentApiTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="dummy", password="dummy_pw")
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_2_1_should_get_all_segments(self):
+        create_segment(segment_name="SUV")
+        create_segment(segment_name="Sedan")
+        res = self.client.get(SEGMENTS_URL)
+        segments = Segment.objects.all().order_by("id")
+        serializer = SegmentSerializer(segments, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+
